cabde1356c9473ee94df1c95f8f78d86e90e794e
Ren Ant <eurekavixen@gmail.com>
2024-09-17 10:01:12 +0800

    completed CRUD functions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -8,11 +10,43 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post].permit(:title, :content))
+    @post = Post.new(post_params)
     if @post.save
+      flash[:notice] = 'Post created successfully'
       redirect_to posts_path
     else
+      flash.now[:alert] = 'Post create failed'
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+
+    if @post.update(post_params)
+      flash[:notice] = 'Post updated successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post update failed'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+
+    @post.destroy
+    flash[:notice] = 'Post destroyed successfully'
+    redirect_to posts_path
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
